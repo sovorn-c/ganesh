284116b678b50aa4fa0aaf507e4880d5be084a08
sovorn-c <cheasovorn4@gmail.com>
2026-09-09 23:38:20 +0700

    feat(e01): add runtime baseline entry point

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+export interface RuntimeBaseline {
+  readonly name: string;
+  readonly version: string;
+}
+
+export const runtimeBaseline: RuntimeBaseline = {
+  name: "ganesh",
+  version: "0.1.0"
+};
